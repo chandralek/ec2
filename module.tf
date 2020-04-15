b76c750acb2759c2142568539128c4c3d97238e9
Chandralekha.s <chandralekha.s@sophos.com>
2020-04-15 22:53:57 +0530

    updating ec2 terraform repo

--- module.tf
+++ module.tf
@@ -1,7 +1,7 @@
 module "ec2" {
   source          = "git::https://chandralekha882:Opcqa123!@github.com/chandralek/modules.git//ec2"
-  PUBLIC_SUBNETS  = data.terraform_remote_state.VPC.output.PUBLIC_SUBNETS
-  PRIVATE_SUBNETS = data.terraform_remote_state.VPC.output.PRIVATE_SUBNETS
-  VPC_ID          = data.terraform_remote_state.VPC.output.VPC_ID
-  MGMT_VPC_ID     = data.terraform_remote_state.VPC.output.MGMT_VPC_ID
+  PUBLIC_SUBNETS  = data.terraform_remote_state.VPC.outputs.PUBLIC_SUBNETS
+  PRIVATE_SUBNETS = data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS
+  VPC_ID          = data.terraform_remote_state.VPC.outputs.VPC_ID
+  MGMT_VPC_ID     = data.terraform_remote_state.VPC.outputs.MGMT_VPC_ID
 }
