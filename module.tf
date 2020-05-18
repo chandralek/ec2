ddb8a2630157eb955fffb9563ec8b607e69955a8
Chandralekha.s <chandralekha.s@sophos.com>
2020-05-18 07:38:35 +0530

    Updating ec2 module

--- module.tf
+++ module.tf
@@ -52,7 +52,7 @@ module "cart-service" {
   HOSTED_ZONE_NAME=data.terraform_remote_state.route53.outputs.HOSTED_ZONE_NAME
 }
 
-/*module "payment-service" {
+module "payment-service" {
   source          = "git::https://chandralekha882:Opcqa123!@github.com/chandralek/modules.git//application-service"
   PUBLIC_SUBNETS  = data.terraform_remote_state.VPC.outputs.PUBLIC_SUBNETS
   PRIVATE_SUBNETS = data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS
@@ -88,6 +88,24 @@ module "dispatch-service" {
   HOSTED_ZONE_NAME=data.terraform_remote_state.route53.outputs.HOSTED_ZONE_NAME
 }
 
+module "shipping-service" {
+  source          = "git::https://chandralekha882:Opcqa123!@github.com/chandralek/modules.git//application-service"
+  PUBLIC_SUBNETS  = data.terraform_remote_state.VPC.outputs.PUBLIC_SUBNETS
+  PRIVATE_SUBNETS = data.terraform_remote_state.VPC.outputs.PRIVATE_SUBNETS
+  VPC_ID          = data.terraform_remote_state.VPC.outputs.VPC_ID
+  MGMT_VPC_ID     = data.terraform_remote_state.VPC.outputs.MGMT_VPC_ID
+  INSTANCE_TYPE   = var.INSTANCE_TYPE
+  TAGS            = var.TAGS
+  GIT_USR         = var.GIT_USR
+  GIT_PSW         = var.GIT_PSW
+  SSH_USR         = var.SSH_USR
+  SSH_PSW         = var.SSH_PSW
+  PORTS           = var.PORTS
+  APPLICATION_NAME= "shipping"
+  HOSTED_ZONE_ID  = data.terraform_remote_state.route53.outputs.HOSTED_ZONE_ID
+  HOSTED_ZONE_NAME=data.terraform_remote_state.route53.outputs.HOSTED_ZONE_NAME
+}
+/*
 module "ratings-service" {
   source          = "git::https://chandralekha882:Opcqa123!@github.com/chandralek/modules.git//application-service"
   PUBLIC_SUBNETS  = data.terraform_remote_state.VPC.outputs.PUBLIC_SUBNETS
